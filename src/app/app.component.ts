8c208c8915a13a1ccbc2dfd8108308531b1766a5
manukam <maduranga.manuka3@gmail.com>
2019-04-09 04:17:19 +0530

    progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,8 @@ import { CrickBackService } from '../app/crick-back.service';
 })
 export class AppComponent {
 
-  constructor(public rest: CrickBackService) { 
-    this.getAllPlayers();    
+  constructor(public rest: CrickBackService) {
+    this.getAllPlayers();
   }
 
   title = 'CrickMLFront';
@@ -19,11 +19,12 @@ export class AppComponent {
 
   playerPool: any = [];
 
-  selectedPlayers = [
-  ];
+  selectedPlayers: any = [];
+
+  players: any = [];
 
   dropped(event: CdkDragDrop<string[]>) {
-    console.log('triggered');
+    // console.log(event);
     if (event.previousContainer === event.container) {
       moveItemInArray(
         event.container.data,
@@ -37,13 +38,26 @@ export class AppComponent {
         event.previousIndex,
         event.currentIndex
       );
+      // console.log(event.container.data);
+      this.selectedPlayers = event.container.data;
     }
   }
 
   getAllPlayers() {
     this.rest.getPlayers().subscribe((data: {}) => {
-      console.log(data);
+      // console.log(data);
       this.playerPool = data;
     });
+  }
+
+  analysePlayers() {
+    this.selectedPlayers.forEach(element => {
+      this.players.push(element.id);
+    });
+
+    this.rest.postPlayers(this.players).subscribe((data: {}) => {
+      console.log(data);
+    });
+
   }
 }
--- src/app/crick-back.service.ts
+++ src/app/crick-back.service.ts
@@ -10,34 +10,41 @@ export class CrickBackService {
 
   constructor(private http: HttpClient) { }
 
-readonly endpoint = 'http://127.0.0.1:5000/players';
-readonly httpOptions = {
+  readonly endpoint = 'http://127.0.0.1:5000/';
+  readonly httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json'
     })
-};
-
-private extractData(res: Response) {
-  const body = res;
-  return body || {};
-}
-
-getPlayers() : Observable < any > {
-    return this.http.get(this.endpoint).pipe(
-      map(this.extractData));
   };
 
-private handleError<T>(operation = 'operation', result ? : T) {
-  return (error: any): Observable<T> => {
+  private extractData(res: Response) {
+    const body = res;
+    return body || {};
+  }
 
-    // TODO: send the error to remote logging infrastructure
-    console.error(error); // log to console instead
+  getPlayers(): Observable<any> {
+    return this.http.get(this.endpoint + 'players').pipe(
+      map(this.extractData));
+  }
 
-    // TODO: better job of transforming error for user consumption
-    console.log(`${operation} failed: ${error.message}`);
+  postPlayers(players): Observable<any> {
+    return this.http.post<any>(this.endpoint + 'selectedPlayers/', JSON.stringify(players), this.httpOptions).pipe(
+      tap((player) => console.log(`added player w/ id=${player.id}`)),
+      catchError(this.handleError<any>('addPlyer'))
+    );
+  }
 
-    // Let the app keep running by returning an empty result.
-    return of(result as T);
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
     };
   }
 }
